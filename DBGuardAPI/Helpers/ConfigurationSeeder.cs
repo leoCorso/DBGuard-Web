b281424733087c8aa5edf531c7ccd4e9602fbca0
Leo Corso <leocodes@outlook.com>
2026-05-14 10:21:54 -0700

    Modified configurator

--- DBGuardAPI/Helpers/ConfigurationSeeder.cs
+++ DBGuardAPI/Helpers/ConfigurationSeeder.cs
@@ -6,11 +6,11 @@ namespace DBGuardAPI.Helpers
     public static class ConfigurationSeeder // Seeds some default env values and inject values into configuration for use in app
     {
         // Db keys
-        private static readonly string DBHostKey = "database_host";
-        private static readonly string DBHPortKey = "database_port";
-        private static readonly string DBNameKey = "database_name";
-        private static readonly string DBUserIdKey = "database_username";
-        private static readonly string DBPasswordKey = "database_password";
+        private static readonly string DBHostKey = "DATABASE_HOST";
+        private static readonly string DBHPortKey = "DATABASE_PORT";
+        private static readonly string DBNameKey = "DATABASE_NAME";
+        private static readonly string DBUserIdKey = "DATABASE_USER";
+        private static readonly string DBPasswordKey = "DATABASE_PASSWORD";
 
         // Default db values
         private static readonly string DefaultDBHost = "postgres"; // Use for docker prod
@@ -22,33 +22,36 @@ namespace DBGuardAPI.Helpers
         private static readonly string DefaultDBPassword = "abc123##";
 
         // Default jwt keys
-        private static readonly string JWTSecurityKeyKey = "jwt_key";
-        private static readonly string JwtIssuerKey = "jwt_issuer";
-        private static readonly string JwtAudienceKey = "jwt_audience";
-        private static readonly string AccessTokenExpirationKey = "jwt_access_token_expiration";
-        private static readonly string  RefreshTokenExpirationKey = "jwt_refresh_token_expiration";
+        private static readonly string JWTSecurityKeyKey = "JWT_KEY";
+        private static readonly string JwtIssuerKey = "JWT_ISSUER";
+        private static readonly string JwtAudienceKey = "JWT_AUDIENCE";
+        private static readonly string AccessTokenExpirationKey = "JWT_ACCESS_TOKEN_EXPIRATION";
+        private static readonly string  RefreshTokenExpirationKey = "JWT_REFRESH_TOKEN_EXPIRATION";
 
         // Default jwt values
         private static readonly string DefaultKeyFilePath = "/app/data/jwt_secret.key"; // Use for docker prod
         //private static readonly string DefaultKeyFilePath = "./jwt_secret.key"; // Use in dev windows
 
-        private static readonly string DefaultJwtIssuer = "http://localhost:5000";
-        private static readonly string DefaultJwtAudience = "http://localhost:5000";
         private static readonly string DefaultTokenExpiration = "15";
         private static readonly string DefaultRefreshTokenExpiration = "1440";
 
         // Admin user keys
-        private static readonly string UsernameKey = "AppUsername";
-        private static readonly string PasswordKey = "Password";
+        private static readonly string UsernameKey = "APP_USERNAME";
+        private static readonly string PasswordKey = "PASSWORD";
 
         // Admin user values
         private static readonly string Username = "admin";
         private static readonly string Password = "Admin123!";
 
         // Cors key
-        private static readonly string DefaultCorsKey = "allowed-origins";
-        // Cors default value
-        private static readonly string DefaultCors = "http://localhost:5000";
+        private static readonly string DefaultCorsKey = "ALLOWED_ORIGINS";
+
+        // Host and port key
+        private static readonly string HostKey = "APP_HOST";
+        private static readonly string PortKey = "APP_PORT";
+        // Host value
+        private static readonly string DefaultHostValue = "localhost";
+        private static readonly string DefaultPortValue = "5000";
 
         public static void SeedDefaultConfiguration(WebApplicationBuilder builder, Serilog.ILogger logger)
         {
@@ -104,8 +107,9 @@ namespace DBGuardAPI.Helpers
         }
         private static void SeedJwtInfo(WebApplicationBuilder builder, Serilog.ILogger logger)
         {
-            string issuer = Environment.GetEnvironmentVariable(JwtIssuerKey) ?? DefaultJwtIssuer;
-            string audience = Environment.GetEnvironmentVariable(JwtAudienceKey) ?? DefaultJwtAudience;
+            string issuer = Environment.GetEnvironmentVariable(JwtIssuerKey) ?? "http://" + GetHost() + ":" + GetPort();
+            string audience = Environment.GetEnvironmentVariable(JwtAudienceKey) ?? "http://" + GetHost() + ":" + GetPort();
+
             string tokenExpiration = Environment.GetEnvironmentVariable(AccessTokenExpirationKey) ?? DefaultTokenExpiration;
             string refreshExpiration = Environment.GetEnvironmentVariable (RefreshTokenExpirationKey) ?? DefaultRefreshTokenExpiration;
             builder.Configuration["JwtSettings:Issuer"] = issuer;
@@ -129,8 +133,16 @@ namespace DBGuardAPI.Helpers
         }
         private static void SeedCorsDefault(WebApplicationBuilder builder, Serilog.ILogger logger)
         {
-            string allowedOrigins = Environment.GetEnvironmentVariable(DefaultCorsKey) ?? DefaultCors;
-            builder.Configuration["Cors:Allowed-Origins"] = allowedOrigins;
+            string defaultCors = "http://" + GetHost() + ":" + GetPort();
+            builder.Configuration["Cors:Allowed-Origins"] = defaultCors + "," + Environment.GetEnvironmentVariable(DefaultCorsKey);
+        }
+        private static string GetHost()
+        {
+            return Environment.GetEnvironmentVariable(HostKey) ?? DefaultHostValue;
+        }
+        private static string GetPort()
+        {
+            return Environment.GetEnvironmentVariable(PortKey) ?? DefaultPortValue;
         }
     }
 }
